55bd3b882a0c4f8c8bc77660d2a4114b806de616
kshitijakarsh <kshitijakarsh@gmail.com>
2025-06-25 21:35:54 +0530

    project section done

--- components/sections/projects.tsx
+++ components/sections/projects.tsx
@@ -8,18 +8,96 @@ export default function Projects() {
       <div className="mt-16 flex justify-center items-center">
         <div className="flex gap-4 items-start">
           <div className="flex-shrink-0">
-            <img 
-              src="./gemini.svg" 
-              alt="project1" 
+            <img
+              src="./gemini.svg"
+              alt="project1"
               className="w-full h-auto max-w-[646px] max-h-[508px] object-contain"
             />
+
+            <h1 className="text-white text-2xl font-bold mt-5">
+              GEMINI SKINCARE
+            </h1>
+            <div className="flex flex-wrap gap-3 mt-4">
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                Copywriting
+              </h1>
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                Brand Content
+              </h1>
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                E-Commerce
+              </h1>
+            </div>
           </div>
           <div className="flex-shrink-0">
-            <img 
-              src="./silv.svg" 
-              alt="project2" 
+            <img
+              src="./silv.svg"
+              alt="project2"
               className="w-full h-auto max-w-[484px] max-h-[508px] object-contain"
             />
+            <h1 className="text-white text-2xl font-bold mt-5">
+              SILV PEANUT BUTTER
+            </h1>
+            <div className="flex flex-wrap gap-3 mt-4">
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                Copywriting
+              </h1>
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                Brand Content
+              </h1>
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                E-Commerce
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-16 flex justify-center items-center">
+        <div className="flex gap-4 items-start">
+          <div className="flex-shrink-0">
+            <img
+              src="./pizza.svg"
+              alt="project1"
+              className="w-full h-auto max-w-[484px] max-h-[508px] object-contain"
+            />
+
+            <h1 className="text-white text-2xl font-bold mt-5">
+              GOODROOT PIZZA
+            </h1>
+            <div className="flex flex-wrap gap-3 mt-4">
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                Copywriting
+              </h1>
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                Brand Content
+              </h1>
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                E-Commerce
+              </h1>
+            </div>
+          </div>
+
+          <div className="flex-shrink-0">
+            <img
+              src="./olipop.svg"
+              alt="project2"
+              className="w-full h-auto max-w-[646px] max-h-[508px] object-contain"
+            />
+            <h1 className="text-white text-2xl font-bold mt-5">
+              OLIPOP DRINKS
+            </h1>
+            <div className="flex flex-wrap gap-3 mt-4">
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                Copywriting
+              </h1>
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                Brand Content
+              </h1>
+              <h1 className="bg-white text-[#1F2B68] px-3 py-1 rounded-full text-sm font-medium shadow-sm border border-gray-200 transition-transform transform duration-200 hover:scale-105">
+                E-Commerce
+              </h1>
+            </div>
           </div>
         </div>
       </div>
